e3312ba07f18698d85ba1be7999e5ad03b6ed58d
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-05 17:59:32 +0200

    Added theoretical comments

---
+++ test.go
@@ -0,0 +1,9 @@
+//Important! - main for executable files, anything else for libraries
+package main
+
+//Access to other packages
+import "fmt"
+
+func main() {
+	fmt.Println("This is a test app")
+}
