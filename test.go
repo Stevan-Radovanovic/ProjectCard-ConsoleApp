44d2318d397003e68f4e0b02ff5c4aeb0d378915
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-05 18:22:18 +0200

    Deleted test.go

--- test.go
+++
@@ -1,9 +0,0 @@
-//Important! - main for executable files, anything else for libraries
-package main
-
-//Access to other packages
-import "fmt"
-
-func main() {
-	fmt.Println("This is a test app")
-}
