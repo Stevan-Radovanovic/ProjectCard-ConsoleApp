3a322863ccfbca2c58a39fe44cb7e18276f91b0e
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-05 19:35:58 +0200

    Added card slice with iteration

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import "fmt"
 
 func main() {
 	//var testCard string = "Attack 7 - Defence 5"
-	card := initializeCard()
-	fmt.Println(card)
+	cards := []string{initializeCard(), initializeCard()}
+	iterateCards(cards)
 }
 
 func initializeCard() string {
 	return "Attack X - Defence X"
+}
+
+func iterateCards(cards []string) {
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
 }
