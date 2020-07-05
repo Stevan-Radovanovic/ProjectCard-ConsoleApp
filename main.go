271df57e681d149f5136765532ad2db9f1e52650
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-05 18:20:44 +0200

    Added initialize function for cards

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var testCard string = "Attack 7 - Defense 5"
-	//card := "...."
-	fmt.Println(testCard)
+	//var testCard string = "Attack 7 - Defence 5"
+	card := initializeCard()
+	fmt.Println(card)
+}
+
+func initializeCard() string {
+	return "Attack X - Defence X"
 }
