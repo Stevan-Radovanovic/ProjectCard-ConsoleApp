cc3f442d6bbc9721e270e11c74fab5d1df37a41e
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-06 18:55:04 +0200

    Lowered times in sleep functions

--- main.go
+++ main.go
@@ -22,29 +22,29 @@ func main() {
 
 	fmt.Println("\nInitializing Ultimate Deck...")
 	cards := initializeUltimateDeck()
-	time.Sleep(4 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	fmt.Println("\nGenerating hands...")
 	deck1, deck2 := cards.generateHand(handsize)
-	time.Sleep(4 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	fmt.Println("\nGame is starting...")
-	time.Sleep(3 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	fmt.Println("Choose your game mode! A/a for Attack, D/d for Defence")
 	fmt.Scanf("%v\n", &gameMode)
 	fmt.Println(gameMode)
 
 	fmt.Println("\nConfiguring game mode...")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	for i := 0; i < handsize; i++ {
 		deck1.showDeck()
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 		fmt.Println("\nChoose a card by index: ")
 		fmt.Scanf("%d\n", &cardIndex)
 		fmt.Println("\nYour opponent is choosing...")
-		time.Sleep(3 * time.Second)
+		time.Sleep(1 * time.Second)
 		deck1, card1 = deck1.removeCardByIndex(cardIndex - 1)
 		deck2, card2 = deck2.removeCardByIndex(rand.Intn(currentHandSize))
 		currentHandSize--
@@ -54,11 +54,11 @@ func main() {
 		} else if result == -1 {
 			score2++
 		}
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Println("\nCalculating results...")
-	time.Sleep(4 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	finalValue := score1 - score2
 
